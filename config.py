c8c99f896a632bd75f58b7aab7a8084e485bdc96
Ashish1437 <121593481+Ashish1437@users.noreply.github.com>
2024-03-17 18:45:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,24 +12,24 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # Add Owner Username without @ 
-OWNER_USERNAME = getenv("OWNER_USERNAME","PIYUSH0264")
+OWNER_USERNAME = getenv("OWNER_USERNAME","imStubborn0")
 # Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME" , "DAISY_12_BOT")
+BOT_USERNAME = getenv("BOT_USERNAME" , "Anshika_Music1_Bot")
 # Don't Add style font 
-BOT_NAME = getenv("BOT_NAME" , "DAISY")
+BOT_NAME = getenv("BOT_NAME" , "Anshika")
 #get Your Assistant User name
-ASSUSERNAME = getenv("ASSUSERNAME" , "DAISYASSIT")
-EVALOP = list(map(int, getenv("EVALOP", "6899244704").split()))
+ASSUSERNAME = getenv("ASSUSERNAME" , "Ashu1572")
+EVALOP = list(map(int, getenv("EVALOP", "7031870310").split()))
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", -1002111155780))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002023861339))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6899244704))
+OWNER_ID = int(getenv("OWNER_ID", 7031870310))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -46,8 +46,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DREAMER_BOTS")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/CHAT_DREAMER_BOTS")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Student_Study1_Group")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/PDF_Factory")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
